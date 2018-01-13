63822f6e4058908dcb10c79d2fcd4aa3bbaf5b96
hesse <markus.hesse@mettenmeier.de>
2018-01-13 11:40:00 +0100

    Fixup error when storing the data from combobox

--- brit/dialogs/definitiondialog.py
+++ brit/dialogs/definitiondialog.py
@@ -47,7 +47,7 @@ class DefinitionDialog(Ui_Dialog, QtBaseClass):
         self.editTarget.setText(self.definition.toPlace)
         
     def _backupTypeChanged(self):
-        self.definition.backupType = self.cbbType.itemData(self.cbbType.currentIndex())
+        self.definition.backupType = unicode(self.cbbType.itemData(self.cbbType.currentIndex()).toString())
     
     def _nameChanged(self):
         self.definition.name = unicode(self.editName.text())
