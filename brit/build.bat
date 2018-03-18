0039fe90c1eeb232f14d17a09e10558914443863
hesse <markus.hesse@mettenmeier.de>
2018-03-18 18:04:40 +0100

    Update build script for new disk

--- brit/build.bat
+++ brit/build.bat
@@ -4,10 +4,13 @@ REM
 
 REM To run this, make sure pyinstaller is installed on your system.
 REM This should be possible with 
-REM  python -m  pip install pyinstaller
+REM  python -m  pip install --trusted-host pypi.python.org pyinstaller
+REM
+REM Furthermore the python header files must exist. For this install 
+REM "python-devel" via OSG 4W setup.
 REM
 
-SET OSG_BAT=e:\QGIS_Dev\QGIS\OSGeo4W.bat
+SET OSG_BAT=C:\QGIS_Dev\QGIS\OSGeo4W.bat
 
 SET SCRIPT_DIR=%~dp0
 
