36ace71d751236c667e3c04fd3ea3f19012f7d4c
hesse <markus.hesse@mettenmeier.de>
2018-04-08 09:31:21 +0200

    Add attributes to retain strategy

--- brit/retain_strategy.py
+++ brit/retain_strategy.py
@@ -4,26 +4,57 @@ Created on 28.12.2017
 @author: hesse
 '''
 
+from datetime import timedelta
+
 class RetainStrategy(object):
     '''
-    classdocs
+    Class to manage the retain strategy of a backup task.
+    If a strategy is attached to a backup task, all backup files not matching the strategy will be deleted.
     '''
+    
+    # List of possible retain intervals    
+    RETAIN_INTERVALS= set(['year', 'month', 'week', 'day'])
 
 
-    def __init__(self, name):
+    def __init__(self, name="", retainDuration=timedelta(microseconds=0), retainInterval="year"):
         '''
         Constructor
         '''
-        self.name = name
+        if retainDuration == None:
+            retainDuration = timedelta(microseconds=0)
+            
+        if retainInterval == None:
+            retainInterval = "year"
+        
+        assert retainInterval in self.RETAIN_INTERVALS
+        
+        self.name           = name
+        # Any backup file not older than this will be retained.
+        self.retainDuration = retainDuration
+        
+        # The last backup of each interval in the past will be retained.
+        self.retainInterval = retainInterval
         
     
     @classmethod
     def fromJson(cls, jsonobj):
-        return cls(jsonobj['name'])
+        if 'retainDuration' in jsonobj:
+            duration = timedelta(seconds=jsonobj['retainDuration'])
+        else:    
+            duration = None
+            
+        if 'retainInterval' in jsonobj:
+            interval = jsonobj['retainInterval']
+        else:
+            interval = None
+            
+        return cls(jsonobj['name'], duration, interval)
            
     
     def toJson(self):
-        values = self.__dict__
-        values['class_name'] = 'RetainStrategy'
+        values = self.__dict__.copy()
+        values.pop('retainDuration', None)
+        values['retainDuration'] = self.retainDuration.total_seconds()
+        values['class_name']     = 'RetainStrategy'
         
         return values
--- brit/tests/testretain_strategy.py
+++ brit/tests/testretain_strategy.py
@@ -5,6 +5,7 @@ Created on 29.12.2017
 '''
 import unittest
 
+from datetime import timedelta
 from brit.retain_strategy import RetainStrategy
 
 
@@ -14,18 +15,38 @@ class TestStrorageStrategy(unittest.TestCase):
     def testCreate(self):
         newSS = RetainStrategy('myName')
         self.assert_(newSS <> None, 'No task create')
-        self.assert_(newSS.name == 'myName', 'Name not OK')   
+        self.assert_(newSS.name == 'myName', 'Name not OK') 
+        self.assert_(newSS.retainDuration == timedelta(microseconds=0), "Default retain duration not OK")  
+        
+        newSS = RetainStrategy('myName', timedelta(days=7))
+        self.assert_(newSS.retainDuration == timedelta(days=7), "Given retain duration not taken")
+        
+        newSS = RetainStrategy('myName', timedelta(days=7), "week")
+        self.assert_(newSS.retainInterval == "week", "Given retain interval not taken")
         
         
     def testFromJson(self):
         newSS = RetainStrategy.fromJson({
             "name": "myName",  
-            "class_name": "RetainStrategy", 
+            "class_name": "RetainStrategy"
             })
         
         self.assert_(newSS <> None, 'No task created')
         self.assert_(newSS.name == 'myName', 'Name not OK')
+        self.assert_(newSS.retainDuration == timedelta(seconds=0), "Default retain duration not taken")
+        self.assert_(newSS.retainInterval == "year", "Default retain interval not taken")
         
+        newSS = RetainStrategy.fromJson({
+            "name": "myName",  
+            "class_name": "RetainStrategy", 
+            "retainInterval": "week",
+            "retainDuration": timedelta(days=7).total_seconds()
+            })
+        
+        self.assert_(newSS <> None, 'No task created')
+        self.assert_(newSS.name == 'myName', 'Name not OK')
+        self.assert_(newSS.retainDuration == timedelta(days=7), "Given retain duration not taken")
+        self.assert_(newSS.retainInterval == "week", "Given retain interval not taken")
         
     def testToJson(self):
         newSS = RetainStrategy('myName')
@@ -34,7 +55,17 @@ class TestStrorageStrategy(unittest.TestCase):
         
         self.assert_(json <> None, 'No json created')
         self.assert_(json['class_name'] == 'RetainStrategy',  'class_name not OK')
-        self.assert_(json['name']       == 'myName',            'name not OK')             
+        self.assert_(json['name']       == 'myName',            'name not OK')  
+        
+        newSS = RetainStrategy('myName', timedelta(days=7), "week")
+        
+        json = newSS.toJson()
+        
+        self.assert_(json <> None, 'No json created')
+        self.assert_(json['class_name']     == 'RetainStrategy', 'class_name not OK')
+        self.assert_(json['name']           == 'myName',         'name not OK')  
+        self.assert_(json['retainInterval'] == 'week',           'Given retain interval not taken')  
+        self.assert_(json['retainDuration'] == timedelta(days=7).total_seconds(), 'Given retain duration not taken')         
 
 
 if __name__ == "__main__":
